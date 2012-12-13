f5a4190646ab16154dfbb4c12b62a39794e88bb8
Art Hornsby <arthornsby@gmail.com>
2012-12-12 19:48:02 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DriverBlog::Application.config.secret_token = 'a9609fbc6f673907311517b117fff72037a33e91d18868b019b0c3314147ee964696fcef21ebeb7119fa5b85dc1a4f6ec92c87b204e23c16b3cfc47def58dd9c'
